6e3f6d2417c45fc9cd640202b194703c77a62d3b
Edward Rodriguez <rodriguez.avery@gmail.com>
2023-07-13 12:02:11 -0500

    add arguements to the team class and first test in spec

--- lib/team.rb
+++ lib/team.rb
@@ -1,2 +1,11 @@
 require "rspec"
 require "./lib/player"
+
+class Team
+  attr_reader :roster, :player
+
+  def initialize(roster, player)
+    @roster = []
+    @player = player
+  end
+end
--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -1,1 +1,11 @@
 require "./lib/team"
+
+describe Team do
+  describe "initialize" do
+    it "is an instance of" do
+      team = Team.new("Dodgers", "Los Angeles")
+
+      expect(team).to be_a(Team)
+    end
+  end
+end
