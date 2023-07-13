d9f0f7bb5e91f5c0bd5a769884d03ca9ed788f73
Edward Rodriguez <rodriguez.avery@gmail.com>
2023-07-13 16:04:35 -0500

    add every method and test through completion

--- lib/team.rb
+++ lib/team.rb
@@ -15,4 +15,57 @@ class Team
   def add_player(new_player)
     @roster << new_player
   end
+
+  def long_term_players
+    @roster.find_all do |player|
+      player.contract_length > 24
+    end
+  end
+
+  def short_term_players
+    players = @roster.find_all do |player|
+      if player.contract_length <= 24
+        player
+      end
+    end
+
+    players.sort_by do |player|
+      player.contract_length
+    end
+  end
+
+  def total_value
+    total = 0
+    @roster.sum do |player|
+      total += player.total_cost
+    end
+    total
+  end
+
+  def details
+    {
+      "total_value" => total_value,
+      "player_count" => player_count
+    }
+  end
+
+  def average_cost_of_player
+    average_cost = (total_value / player_count).digits
+
+    cost = ""
+
+    average_cost.each.with_index do |digit, index|
+      cost = digit.to_s + cost
+
+      if index % 3 == 2
+        cost = "," + cost
+      end
+    end
+
+    "$#{cost}"
+  end
+
+  def players_by_last_name
+    @roster.map { |player| player.last_name }.sort.join(", ")
+  end
 end
--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -30,12 +30,107 @@ describe Team do
 
         player_1 = Player.new("Michael Palledorous", 1000000, 36)
         player_2 = Player.new("Kenny DeNunez", 500000, 24)
+        player_3 = Player.new("Alan McClennan", 750000, 48)
+        player_4 = Player.new("Hamilton Porter", 100000, 12)
 
         team.add_player(player_1)
         team.add_player(player_2)
 
         expect(team.roster).to eq([player_1, player_2])
         expect(team.player_count).to eq(2)
+
+        team.add_player(player_3)
+        team.add_player(player_4)
+      end
+    end
+
+    describe "#long_term_players" do
+      it "returns a list of players whose contract is greater than 2 years" do
+        team = Team.new("Dodgers", "Los Angeles")
+
+        player_1 = Player.new("Michael Palledorous", 1000000, 36)
+        player_2 = Player.new("Kenny DeNunez", 500000, 24)
+        player_3 = Player.new("Alan McClennan", 750000, 48)
+        player_4 = Player.new("Hamilton Porter", 100000, 12)
+
+        team.add_player(player_1)
+        team.add_player(player_2)
+        team.add_player(player_3)
+        team.add_player(player_4)
+
+        expect(team.long_term_players).to eq([player_1, player_3])
+      end
+    end
+
+    describe "#short_term_players" do
+      it "returns a list of players whose contract is 2 years or less" do
+        team = Team.new("Dodgers", "Los Angeles")
+
+        player_1 = Player.new("Michael Palledorous", 1000000, 36)
+        player_2 = Player.new("Kenny DeNunez", 500000, 24)
+        player_3 = Player.new("Alan McClennan", 750000, 48)
+        player_4 = Player.new("Hamilton Porter", 100000, 12)
+
+        team.add_player(player_1)
+        team.add_player(player_2)
+        team.add_player(player_3)
+        team.add_player(player_4)
+
+        expect(team.short_term_players).to eq([player_4, player_2])
+      end
+    end
+
+    describe "#total_value" do
+      it "totals the total_cost of players" do
+        team = Team.new("Dodgers", "Los Angeles")
+
+        player_1 = Player.new("Michael Palledorous", 1000000, 36)
+        player_2 = Player.new("Kenny DeNunez", 500000, 24)
+        player_3 = Player.new("Alan McClennan", 750000, 48)
+        player_4 = Player.new("Hamilton Porter", 100000, 12)
+
+        team.add_player(player_1)
+        team.add_player(player_2)
+        team.add_player(player_3)
+        team.add_player(player_4)
+
+        expect(team.total_value).to eq(85200000)
+      end
+    end
+
+    describe "#details" do
+      it "shows all details, total_value and player_count" do
+        team = Team.new("Dodgers", "Los Angeles")
+
+        player_1 = Player.new("Michael Palledorous", 1000000, 36)
+        player_2 = Player.new("Kenny DeNunez", 500000, 24)
+        player_3 = Player.new("Alan McClennan", 750000, 48)
+        player_4 = Player.new("Hamilton Porter", 100000, 12)
+
+        team.add_player(player_1)
+        team.add_player(player_2)
+        team.add_player(player_3)
+        team.add_player(player_4)
+
+        expect(team.details).to eq({"total_value" => 85200000, "player_count" => 4})
+      end
+    end
+
+    describe "#players_by_last_name" do
+      it "shows all players sorted by last_name" do
+        team = Team.new("Dodgers", "Los Angeles")
+
+        player_1 = Player.new("Michael Palledorous", 1000000, 36)
+        player_2 = Player.new("Kenny DeNunez", 500000, 24)
+        player_3 = Player.new("Alan McClennan", 750000, 48)
+        player_4 = Player.new("Hamilton Porter", 100000, 12)
+
+        team.add_player(player_1)
+        team.add_player(player_2)
+        team.add_player(player_3)
+        team.add_player(player_4)
+
+        expect(team.players_by_last_name).to eq("DeNunez, McClennan, Palledorous, Porter")
       end
     end
   end
