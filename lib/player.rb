5e017d5e3d62390a1312434d4a85585f1fe725ed
Edward Rodriguez <rodriguez.avery@gmail.com>
2023-07-13 10:49:25 -0500

    add the player class and firstname method

---
+++ lib/player.rb
@@ -0,0 +1,11 @@
+class Player
+  def initialize(name, monthly_cost, contract_length)
+    @name = name
+    @monthly_cost = monthly_cost
+    @contract_length = contract_length
+  end
+
+  def first_name
+    first_name = name.index(0)
+  end
+end
